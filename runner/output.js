c51101cae5c326b90c36ae5470e5a35253ed763a
Benjie Gillam <benjie@jemjie.com>
2018-02-24 16:56:59 +0000

    Colours

--- runner/output.js
+++ runner/output.js
@@ -1,5 +1,6 @@
 const results = require("./results.json");
 const { table } = require("table");
+const chalk = require("chalk");
 
 const data = [
   [
@@ -14,24 +15,26 @@ const data = [
     "latency p90",
     "latency p99",
     "max RSS"
-  ]
+  ].map(str => chalk.bold(str))
 ];
 
 for (const result of results) {
   const [bin, query, concurrency] = result.title.split(/ \/ (?:concurrency=)?/);
-  data.push([
-    query,
-    bin,
-    concurrency,
-    result.requests.sent,
-    result.requests.total,
-    result.requests.average,
-    result.latency.average,
-    result.latency.p50,
-    result.latency.p90,
-    result.latency.p99,
-    Math.max(...result.memorySamples.map(([ts, rss]) => rss))
-  ]);
+  data.push(
+    [
+      query,
+      (bin === "postgraphile" ? chalk.green : chalk.red)(bin),
+      concurrency,
+      result.requests.sent,
+      result.requests.total,
+      result.requests.average,
+      result.latency.average,
+      result.latency.p50,
+      result.latency.p90,
+      result.latency.p99,
+      Math.max(...result.memorySamples.map(([ts, rss]) => rss))
+    ].map(String)
+  );
 }
 
 console.log(table(data));
