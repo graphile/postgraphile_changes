88874d5f8b3deab094f1311c1a2aedc97779eef1
Benjie Gillam <benjie@jemjie.com>
2018-02-24 17:41:38 +0000

    Script to generate schema diff

---
+++ schema_dump.sh
@@ -0,0 +1,15 @@
+npm run postgraphql -- --export-schema-graphql schema/v3-schema.graphql &
+PID=$!
+sleep 2
+kill $PID
+wait
+
+npm run postgraphile -- --export-schema-graphql schema/v4-schema.graphql &
+PID=$!
+sleep 2
+kill $PID
+wait
+
+./schema/sort-schema.js schema/v3-schema.graphql > schema/v3-schema.sorted.graphql
+./schema/sort-schema.js schema/v4-schema.graphql > schema/v4-schema.sorted.graphql
+diff -ur schema/v3-schema.sorted.graphql schema/v4-schema.sorted.graphql > schema/schema.graphql.diff
